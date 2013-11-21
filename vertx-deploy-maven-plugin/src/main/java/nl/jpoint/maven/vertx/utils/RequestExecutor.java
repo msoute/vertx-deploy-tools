04eeb2b6003ef78967faf04500dabd48bad246fe
Marcel Soute <m.soute@gmail.com>
2013-11-21 08:47:06 +0100

    Fail build if response status from module != 200

--- vertx-deploy-maven-plugin/src/main/java/nl/jpoint/maven/vertx/utils/RequestExecutor.java
+++ vertx-deploy-maven-plugin/src/main/java/nl/jpoint/maven/vertx/utils/RequestExecutor.java
@@ -7,6 +7,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.BasicHttpEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
 
 import java.io.ByteArrayInputStream;
@@ -22,25 +23,33 @@ public class RequestExecutor {
         this.log = log;
     }
 
-    public void executeDeployRequests(DeployConfiguration activeConfiguration, List<Request> requestList) {
+    public void executeDeployRequests(DeployConfiguration activeConfiguration, List<Request> requestList) throws MojoExecutionException {
 
         for (String host : activeConfiguration.getHosts()) {
-            CloseableHttpClient httpClient = HttpClients.createDefault();
 
-            for (Request request : requestList) {
-                HttpPost post = new HttpPost(host + request.getEndpoint());
+            try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
 
-                ByteArrayInputStream bos = new ByteArrayInputStream(request.toJson().getBytes());
-                BasicHttpEntity entity = new BasicHttpEntity();
-                entity.setContent(bos);
-                entity.setContentLength(request.toJson().getBytes().length);
-                post.setEntity(entity);
+                for (Request request : requestList) {
+                    HttpPost post = new HttpPost(host + request.getEndpoint());
 
-                try (CloseableHttpResponse response = httpClient.execute(post)) {
-                    log.info("DeployModuleCommand : Post response status " + response.getStatusLine().getStatusCode());
-                } catch (IOException e) {
-                    log.error("testDeployModuleCommand ", e);
+                    ByteArrayInputStream bos = new ByteArrayInputStream(request.toJson().getBytes());
+                    BasicHttpEntity entity = new BasicHttpEntity();
+                    entity.setContent(bos);
+                    entity.setContentLength(request.toJson().getBytes().length);
+                    post.setEntity(entity);
+
+                    try (CloseableHttpResponse response = httpClient.execute(post)) {
+                        log.info("DeployModuleCommand : Post response status " + response.getStatusLine().getStatusCode());
+                        if (response.getStatusLine().getStatusCode() != 200) {
+                            throw new MojoExecutionException("Error deploying module. ");
+                        }
+                    } catch (IOException e) {
+                        log.error("testDeployModuleCommand ", e);
+                    }
                 }
+            } catch (IOException e) {
+                log.error("testDeployModuleCommand ", e);
+                throw new MojoExecutionException("Error deploying module.", e);
             }
 
         }
