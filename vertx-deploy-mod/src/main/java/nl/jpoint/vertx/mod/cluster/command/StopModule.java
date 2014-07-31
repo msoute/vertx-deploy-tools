b6a97d245c24d72d0cdfecbc54acaefab786af2b
msoute <m.soute@gmail.com>
2014-08-01 00:42:00 +0200

    Add logging to stop process and use sudo

--- vertx-deploy-mod/src/main/java/nl/jpoint/vertx/mod/cluster/command/StopModule.java
+++ vertx-deploy-mod/src/main/java/nl/jpoint/vertx/mod/cluster/command/StopModule.java
@@ -9,8 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.vertx.java.core.Vertx;
 import org.vertx.java.core.json.JsonObject;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class StopModule implements Command<ModuleRequest> {
 
@@ -27,17 +29,31 @@ public class StopModule implements Command<ModuleRequest> {
     public JsonObject execute(ModuleRequest request) {
         Process killProcess;
         final JsonObject result = new JsonObject();
+        result.putBoolean(Constants.STOP_STATUS, false);
 
         for (String file : modRoot.list(new ModuleFileNameFilter(request))) {
             LOG.info("[{} - {}]: Stopping module {}", LogConstants.DEPLOY_REQUEST, request.getId(), file);
 
             try {
-                killProcess = Runtime.getRuntime().exec(new String[]{"/etc/init.d/vertx", "stop-module", file});
+                killProcess = Runtime.getRuntime().exec(new String[]{"sudo", "/etc/init.d/vertx", "stop-module", file});
                 killProcess.waitFor();
+                int exitValue = killProcess.exitValue();
+                BufferedReader output = new BufferedReader(new InputStreamReader(killProcess.getInputStream()));
+                String outputLine;
+                while ((outputLine = output.readLine()) != null) {
+                    LOG.info("[{} - {}]: {}", LogConstants.DEPLOY_REQUEST, request.getId(), outputLine);
+                }
+
+                if (exitValue != 0) {
+                    BufferedReader errorOut = new BufferedReader(new InputStreamReader(killProcess.getErrorStream()));
+                    String errorLine;
+                    while ((errorLine = errorOut.readLine()) != null) {
+                        LOG.error("[{} - {}]: {}", LogConstants.DEPLOY_REQUEST, request.getId(), errorLine);
+                    }
+                }
                 result.putBoolean(Constants.STOP_STATUS, true);
             } catch (IOException | InterruptedException e) {
                 LOG.error("[{} - {}]: Failed to stop module {}", LogConstants.DEPLOY_REQUEST, request.getId(), request.getModuleId());
-                result.putBoolean(Constants.STOP_STATUS, false);
                 return result;
             }
         }
