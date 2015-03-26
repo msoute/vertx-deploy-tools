65f9175d3f93281856d57250c77252277f1016b6
msoute <m.soute@gmail.com>
2015-03-26 20:58:49 +0100

    Ignore test

--- vertx-deploy-maven-plugin/src/test/java/nl/jpoint/maven/vertx/utils/AwsEc2UtilTest.java
+++ vertx-deploy-maven-plugin/src/test/java/nl/jpoint/maven/vertx/utils/AwsEc2UtilTest.java
@@ -5,6 +5,7 @@ import org.apache.maven.monitor.logging.DefaultLog;
 import org.apache.maven.plugin.logging.Log;
 import org.codehaus.plexus.logging.Logger;
 import org.codehaus.plexus.logging.console.ConsoleLogger;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.List;
@@ -17,7 +18,7 @@ public class AwsEc2UtilTest {
     private static final Log LOG = new DefaultLog(new ConsoleLogger(Logger.LEVEL_DEBUG, "logger"));
 
     @Test
-
+    @Ignore
     public void testDescribeInstances() throws Exception {
 
         DeployConfiguration config = new DeployConfiguration();
@@ -40,6 +41,7 @@ public class AwsEc2UtilTest {
     }
 
     @Test
+    @Ignore
     public void testLabelCheck() throws AwsException {
         AwsAutoScalingUtil awsAutoScalingUtil = new AwsAutoScalingUtil(AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY);
         AutoScalingGroup autoScalingGroup = awsAutoScalingUtil.describeAutoScalingGroup(AWS_AS_GROUP, LOG);
