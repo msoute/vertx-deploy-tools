9c6dd672dd67447bf46ccee87c08ae7a97b5a39b
msoute <m.soute@gmail.com>
2015-08-14 13:18:41 +0200

    * Bump source level to 1.8 * Use  Aws SDK for autoscaling groups

--- vertx-deploy-maven-plugin/src/main/java/nl/jpoint/maven/vertx/utils/AwsDeployUtils.java
+++ vertx-deploy-maven-plugin/src/main/java/nl/jpoint/maven/vertx/utils/AwsDeployUtils.java
@@ -1,11 +1,15 @@
 package nl.jpoint.maven.vertx.utils;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.autoscaling.AmazonAutoScalingClient;
+import com.amazonaws.services.autoscaling.model.AutoScalingGroup;
 import com.amazonaws.services.autoscaling.model.DescribeAutoScalingGroupsRequest;
 import com.amazonaws.services.autoscaling.model.DescribeAutoScalingGroupsResult;
+import com.amazonaws.services.autoscaling.model.Instance;
+import com.amazonaws.services.autoscaling.model.LifecycleState;
 import com.amazonaws.services.autoscaling.model.ResumeProcessesRequest;
 import com.amazonaws.services.autoscaling.model.SuspendProcessesRequest;
 import com.amazonaws.services.autoscaling.model.UpdateAutoScalingGroupRequest;
@@ -17,10 +21,10 @@ import org.apache.maven.settings.Server;
 import org.apache.maven.settings.Settings;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AwsDeployUtils {
 
-    private final AwsAutoScalingUtil awsAutoScalingUtil;
     private final AwsEc2Util awsEc2Util;
     private final AwsOpsWorksUtil opsWorksUtil;
     private final AwsElbUtil awsElbUtil;
@@ -37,7 +41,6 @@ public class AwsDeployUtils {
         awsAsClient = new AmazonAutoScalingClient(new BasicAWSCredentials(server.getUsername(), server.getPassword()));
         awsAsClient.setRegion(Region.getRegion(Regions.EU_WEST_1));
 
-        awsAutoScalingUtil = new AwsAutoScalingUtil(server.getUsername(), server.getPassword());
         awsEc2Util = new AwsEc2Util(server.getUsername(), server.getPassword());
         opsWorksUtil = new AwsOpsWorksUtil(server.getUsername(), server.getPassword());
         awsElbUtil = new AwsElbUtil(server.getUsername(), server.getPassword());
@@ -74,18 +77,35 @@ public class AwsDeployUtils {
 
         try {
             log.debug("describing Autoscaling group");
-            AutoScalingGroup autoScalingGroup = awsAutoScalingUtil.describeAutoScalingGroup(activeConfiguration.getAutoScalingGroupId(), activeConfiguration.isIgnoreInStandby(), log);
-            if (!autoScalingGroup.deployable() && !activeConfiguration.isIgnoreDeployState()) {
+            DescribeAutoScalingGroupsResult result = awsAsClient.describeAutoScalingGroups(new DescribeAutoScalingGroupsRequest().withAutoScalingGroupNames(activeConfiguration.getAutoScalingGroupId()));
+
+            AutoScalingGroup autoScalingGroup = result.getAutoScalingGroups().get(0);
+
+            if (isDeployable(autoScalingGroup, activeConfiguration.isIgnoreInStandby()) && !activeConfiguration.isIgnoreDeployState()) {
                 throw new MojoExecutionException("Autoscaling group is not in a deployable state.");
             }
             log.debug("describing instances in Autoscaling group");
-            List<Ec2Instance> instances = awsEc2Util.describeInstances(autoScalingGroup.getInstances(), activeConfiguration.getTag(), log);
+            List<Ec2Instance> instances = awsEc2Util.describeInstances(autoScalingGroup.getInstances().stream().map(Instance::getInstanceId).collect(Collectors.toList()), activeConfiguration.getTag(), log);
             log.debug("describing elb status");
-            return awsElbUtil.describeInstanceElbStatus(instances, autoScalingGroup.getElbs());
+            return awsElbUtil.describeInstanceElbStatus(instances, autoScalingGroup.getLoadBalancerNames());
 
-        } catch (AwsException e) {
+        } catch (AwsException | AmazonClientException e) {
             throw new MojoFailureException(e.getMessage());
         }
+    }
+
+    private boolean isDeployable(AutoScalingGroup autoScalingGroup, boolean ignoreInStandby) {
+        long inServiceInstances = autoScalingGroup.getInstances().stream()
+                .filter(i -> i.getLifecycleState().equals(LifecycleState.InService.toString()))
+                .count();
+
+        long inStandbyInstances = autoScalingGroup.getInstances().stream()
+                .filter(i -> i.getLifecycleState().equals(LifecycleState.Standby.toString()))
+                .count();
+
+        long healthyInstances = ignoreInStandby ? inServiceInstances : inServiceInstances + inStandbyInstances;
+
+        return !(autoScalingGroup.getMinSize().equals(1) && autoScalingGroup.getDesiredCapacity().equals(1) && healthyInstances <= 1);
     }
 
     public void getHostsOpsWorks(Log log, DeployConfiguration activeConfiguration) throws MojoFailureException {
