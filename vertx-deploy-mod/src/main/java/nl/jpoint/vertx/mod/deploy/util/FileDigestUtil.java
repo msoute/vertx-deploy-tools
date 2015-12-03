f34302de6b76d3e5aae9b94dd3f277d337f04d5e
Marcel Soute <m.soute@gmail.com>
2015-12-03 09:07:29 +0100

    Logging

--- vertx-deploy-mod/src/main/java/nl/jpoint/vertx/mod/deploy/util/FileDigestUtil.java
+++ vertx-deploy-mod/src/main/java/nl/jpoint/vertx/mod/deploy/util/FileDigestUtil.java
@@ -1,5 +1,8 @@
 package nl.jpoint.vertx.mod.deploy.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -10,18 +13,23 @@ import java.security.NoSuchAlgorithmException;
 
 public class FileDigestUtil {
 
+    private final Logger LOG = LoggerFactory.getLogger(FileDigestUtil.class);
+
+
     private MessageDigest md;
 
     public FileDigestUtil() {
         try {
             md = MessageDigest.getInstance("MD5");
         } catch (NoSuchAlgorithmException e) {
+            LOG.error("Error creating MD instance", e);
             md = null;
         }
     }
 
     public byte[] getFileMd5Sum(Path unpackFile) {
         if (md == null || Files.notExists(unpackFile)) {
+            LOG.error("MD is null {}, or file does not exist {}", md, unpackFile);
             return null;
         }
         try (InputStream is = Files.newInputStream(unpackFile)) {
@@ -31,6 +39,7 @@ public class FileDigestUtil {
             md.reset();
             return digest;
         } catch (IOException e) {
+            LOG.error("Error calculating MD5 sum", e);
             return null;
         }
     }
