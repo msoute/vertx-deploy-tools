2047b7cdfb2b4903ebb7b7b12e0ae1dcf8b71b09
Marcel Soute <m.soute@gmail.com>
2016-02-09 07:27:41 +0100

    Formatting

--- vertx-deploy/src/main/java/nl/jpoint/vertx/mod/deploy/handler/RestDeployHandler.java
+++ vertx-deploy/src/main/java/nl/jpoint/vertx/mod/deploy/handler/RestDeployHandler.java
@@ -163,7 +163,8 @@ public class RestDeployHandler implements Handler<RoutingContext> {
     private Observable<DeployRequest> stopContainer(DeployRequest deployRequest) {
         awsService.ifPresent(aws -> aws.updateAndGetRequest(DeployState.STOPPING_CONTAINER, deployRequest.getId().toString()));
         if (deployRequest.withRestart()) {
-            return applicationApplicationService.stopContainer().flatMap(x -> just(deployRequest));
+            return applicationApplicationService.stopContainer()
+                    .flatMap(x -> just(deployRequest));
         }
         return just(deployRequest);
     }
