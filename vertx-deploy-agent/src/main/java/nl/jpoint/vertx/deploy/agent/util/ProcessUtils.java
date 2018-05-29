f985ee6b59d64db1efac0ee61191fabeda77b48c
Marcel Soute <m.soute@gmail.com>
2018-05-29 15:06:01 +0200

    Do not kill services. Move log line for stopping container. Only log when restart is enabled.
    (cherry picked from commit 1d4478b)
    
    (cherry picked from commit 4b447a1)

--- vertx-deploy-agent/src/main/java/nl/jpoint/vertx/deploy/agent/util/ProcessUtils.java
+++ vertx-deploy-agent/src/main/java/nl/jpoint/vertx/deploy/agent/util/ProcessUtils.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -104,16 +103,5 @@ public class ProcessUtils {
 
     public String getRunningVersion(DeployApplicationRequest request) {
         return listInstalledAndRunningModules().get(request.getMavenArtifactId());
-    }
-
-    public static boolean killService(String service) {
-        ProcessBuilder processKillBuilder = new ProcessBuilder().command(Arrays.asList("/usr/bin/pgrep", "-f", service, "|", "xargs", "kill", "-9"));
-        try {
-            processKillBuilder.start();
-            return true;
-        } catch (IOException e) {
-            LOG.error(e.getMessage());
-            return false;
-        }
     }
 }
